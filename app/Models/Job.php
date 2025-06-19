efe818d6126118d2ea68c43c1174c1571f6ba5f0
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2025-06-20 02:43:34 +0400

    Models, Factories, Migration

---
+++ app/Models/Job.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model {
+    use HasFactory;
+    protected $table = 'jobs_listings';
+
+    protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
+    <h1 class="">Hello from home page</h1>
+</x-layout>
