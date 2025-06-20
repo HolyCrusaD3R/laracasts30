fa564e2b00b8b6d0db4a22d685000f0774b991c6
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2025-06-20 17:03:12 +0400

    add laravel debugbar, use eager loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,13 +2,16 @@
     <x-slot:heading>
         Jobs page
     </x-slot:heading>
-    <ul>
+    <div class="flex flex-col gap-2">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/job/{{ $job['id'] }}">
-                    <strong>{{ $job['title']  }}:</strong> Pays {{ $job['salary'] }} per year.
+            
+                <a href="/job/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                    <div class="font-bold text-blue-500">{{ $job->employer->name  }}</div>
+                    <div>
+                        <strong>{{ $job['title']  }}:</strong> Pays {{ $job['salary'] }} per year.
+                    </div>
                 </a>
-            </li>
+            
         @endforeach
-    </ul>
+    </div>
 </x-layout>
