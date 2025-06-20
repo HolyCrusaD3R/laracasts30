0aebee604278342bd6c630c7961361ef5962ff36
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2025-06-20 19:20:52 +0400

    validation

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -14,8 +14,11 @@
                 <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                 <div class="mt-2">
                     <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Plumber" />
+                    <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Plumber" required/>
                     </div>
+                    @error('title')
+                        <p class="text-xs text-red-500 font-semibold mt-3">{{ $message }}</p>
+                    @enderror
                 </div>
                 </div>
 
@@ -23,15 +26,25 @@
                 <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                 <div class="mt-2">
                     <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="$50,000" />
+                    <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="$50,000" required/>
                     </div>
+                    @error('salary')
+                        <p class="text-xs text-red-500 font-semibold mt-3">{{ $message }}</p>
+                    @enderror
                 </div>
                 </div>
-               
-               
+                
             </div>
-            </div>
-
+            {{-- <div class="text-red-500 mt-10">        
+                @if($errors->any()) 
+                    <div>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </div>
+                @endif
+            </div> --}}
+        </div>
             
 
         </div>
