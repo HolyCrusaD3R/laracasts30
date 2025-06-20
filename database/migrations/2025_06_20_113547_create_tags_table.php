2a57d7855ca40dc16c85142dde1d81a8b859543b
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2025-06-20 17:25:51 +0400

    pagination, vendor publish, fix migration order in create_tags_table down()

--- database/migrations/2025_06_20_113547_create_tags_table.php
+++ database/migrations/2025_06_20_113547_create_tags_table.php
@@ -32,7 +32,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
         Schema::dropIfExists('job_tag');
+        Schema::dropIfExists('tags');
     }
 };
